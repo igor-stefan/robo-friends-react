a435ab4ae28c82650961064f9112a22c973883cf
igor-stefan <igorstefan@bol.com.br>
2020-06-09 19:34:01 -0300

    Updates

---
+++ precache-manifest.dcfe89e28b8fa69e94b79927898b25df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb9d0688e573d9627275d2cee9a2403",
+    "url": "/robo-friends-react/index.html"
+  },
+  {
+    "revision": "f054b87f96b9f893caa8",
+    "url": "/robo-friends-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "08717a5ee5442c21be32",
+    "url": "/robo-friends-react/static/css/main.3eb1d315.chunk.css"
+  },
+  {
+    "revision": "f054b87f96b9f893caa8",
+    "url": "/robo-friends-react/static/js/2.13f860af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends-react/static/js/2.13f860af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08717a5ee5442c21be32",
+    "url": "/robo-friends-react/static/js/main.b7b8be45.chunk.js"
+  },
+  {
+    "revision": "230eb885a011b8f492e7",
+    "url": "/robo-friends-react/static/js/runtime-main.82a80b81.js"
+  },
+  {
+    "revision": "879671aee16871b65b221b25794e6733",
+    "url": "/robo-friends-react/static/media/Earthrealm3D-0AEr.879671ae.woff"
+  },
+  {
+    "revision": "b21224203acb696422b0871b9cbb7595",
+    "url": "/robo-friends-react/static/media/Earthrealm3DItalic-ed7O.b2122420.woff"
+  }
+]);
