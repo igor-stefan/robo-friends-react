24671f814cb438547154128e9343c940bc76baf8
igor-stefan <igorstefan@bol.com.br>
2020-06-10 14:40:17 -0300

    fixed some css lacks

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Scroll = (props) => {
     return(
         <div style = {{overflow:'scroll', borderTop:'1.8px dotted yellow'
-        ,borderBottom:'1.8px dotted yellow', height:'20em'}}>
+        ,borderBottom:'1.8px dotted yellow', height:'30em'}}>
             {props.children}
         </div>
     ) 
